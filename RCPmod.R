9685419ac324d41440d9525f757458b1e1564239
sare-ah <sarah.davies@dfo-mpo.gc.ca>
2018-08-07 14:46:40 -0700

    Added code to make spatial prediction with sampling effect

--- RCPmod.R
+++ RCPmod.R
@@ -28,7 +28,17 @@ setwd('..')
 source("./Scripts/RCP_Helper_Functions.R")
 
 # Make packages available
-UsePackages( pkgs=c("dplyr","RCPmod", "raster", "rasterVis", "tidyr") ) 
+UsePackages( pkgs=c("dplyr","RCPmod", "raster", "rasterVis", "tidyr", "png") ) 
+
+# Controls
+# ========
+enviro.variables <- c("Long_MP", "log_depth", "caisom_floor_temperature" )  # CHANGE to match environmental variables used for BHM
+id_vars="HaulIndex"
+sample_vars="Season"
+
+distribution <- "NegBin"  # Change to "Bernoulli" for P/A data
+gen.start.val <- "random2"
+n.sites <- 181 # Number of sites in study area
 
 # Load the data
 # =============
@@ -46,7 +56,6 @@ rcp_env_vars <- c( "Long_MP", "log_depth", "caisom_floor_temperature" )  # CHANG
 rcp_poly <- poly_data(poly_vars=rcp_env_vars, degree=c(2,2,2), 
                     id_vars="HaulIndex",sample_vars="Season", 
                     species_vars=species, data=covariates.species)
-
 rcp_data <- rcp_poly$rcp_data
 
 # Load rasters & create dataframe of prediction space --- NEED to set environmental layers up to be read in as a raster brick (?I think?)
@@ -58,7 +67,6 @@ pred_space_rcp <- as.data.frame(rasterToPoints(
   subset(pred_masked, c( "Long_MP", "bathymetry", "caisom_floor_temperature" ))))
 pred_space_rcp <- na.omit( pred_space_rcp )
 pred_space_rcp$log_depth <- log( pred_space_rcp$bathymetry* -1 )
-
 
 # ??poly_pred_space()  --- I AM CONFUSED BY THIS FUNCTION
 # Transform using stored polys, predict and plot results
@@ -86,94 +94,104 @@ nRCPs_samp <- list()
 
 # Populate list manually
 nRCPs_samp[[1]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=1,
-                                      inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1)
+                                      inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1)
 nRCPs_samp[[2]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=2,
-                                    inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1) # throws an error!
+                                    inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1) # throws an error!
 nRCPs_samp[[3]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=3,
-                                    inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1)
+                                    inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1)
 nRCPs_samp[[4]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=4,
-                                    inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1)
+                                    inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1)
 nRCPs_samp[[5]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=5,
-                                    inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1)
+                                    inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1)
 nRCPs_samp[[6]] <- regimix.multifit(form.RCP=form, form.spp= ~ Season, data=rcp_data, nRCP=6,
-                                    inits="random2", nstart=nstarts, dist="NegBin", mc.cores=1)
+                                    inits=gen.start.val, nstart=nstarts, dist=distribution, mc.cores=1)
 
-print("here")
 saveRDS(nRCPs_samp, file="nRCPs_samp.rds")
 
 # Determine best model
 # ====================
+# Name list elements & remove elements with null values (model failed to run properly for nRCP=2)
+names(nRCPs_samp) <- seq_along(nRCPs_samp)
+nRCPs_samp[sapply(nRCPs_samp, is.null)] <- NULL
+
 # Get BICs
 RCPsamp_BICs <- sapply( nRCPs_samp, function(x) sapply( x, function(y) y$BIC))
-# If regimix.multifit() failed on one RCP then populate the list element --- FIX to include a test for NULL
-RCPsamp_BICs[2] <- RCPsamp_BICs[1]
-# Are any RCPs consisting of a small number of sites?  (A posteriori) If so remove.
-# CHANGE 181 to number of sites (as a variable)
-# This next line does not populate a list correctly.  Creates a list of 10 elements (should be 6)
-#RCPsamp_minPosteriorSites <- cbind( 181, sapply( nRCPs_samp[-1], function(y) sapply( y, function(x) min( colSums( x$postProbs)))))
-#RCPsamp_ObviouslyBad <- RCPsamp_minPosteriorSites < 2
-#RCPsamp_BICs[RCPsamp_ObviouslyBad] <- NA
 
+# Are any RCPs consisting of a small number of sites?  (A posteriori) If so remove.
+RCPsamp_minPosteriorSites <- cbind( n.sites, sapply( nRCPs_samp[-1], function(y) sapply( y, function(x) min( colSums( x$postProbs)))))
+RCPsamp_ObviouslyBad <- RCPsamp_minPosteriorSites < 2
+RCPsamp_BICs[RCPsamp_ObviouslyBad] <- NA
 
 # Plot minimum BIC for each nRCP
-# RCPsamp_minBICs <- apply( RCPsamp_BICs, 2, min, na.rm=TRUE)
-RCPsamp_minBICs <- lapply(RCPsamp_BICs, min, na.rm=TRUE)
+RCPsamp_minBICs <- apply(RCPsamp_BICs, 2, min, na.rm=TRUE)
 
-bics <- lapply(RCPsamp_BICs, as.numeric)
-df <- as.data.frame(RCPsamp_BICs)
-colnames(df) <- 1:ncol(df)
-
-plot( 1:max.nRCP, RCPsamp_minBICs, type='b', ylab="BIC", xlab="nRCP", pch=20)
-# I can't get the points to plot
-# points( rep( 1:max.nRCP, each=nrow( RCPsamp_BICs)), RCPsamp_BICs, pch=20)
-# points( rep( 1:ncol(df), each=nrow( df)), df, pch=20)
+plot( names(nRCPs_samp), RCPsamp_minBICs, type='b', ylab="BIC", xlab="nRCP", pch=20)
+points( rep(names(nRCPs_samp), each=nrow( RCPsamp_BICs)), RCPsamp_BICs, pch=20)
 
 # Choose best model from above 
-#RCPsamp_goodun <- which.min( RCPsamp_BICs[,3])  # what is the 3 identifying here??
-# set a variables to match the lowest BIC with a number of RCPs
-nRCP_best <- which.min(RCPsamp_minBICs)
-inits <- unlist( nRCPs_samp[[nRCP_best]][[3]]$coef)
+# set variables to match the lowest BIC with a number of RCPs
+nRCP_best <- as.numeric(which.min(RCPsamp_minBICs))
+inits_best <- unlist( nRCPs_samp[[nRCP_best]][[3]]$coef)
 
 # Rerun best model (for full output)
 # ==================================
 control <- list( optimise=FALSE, quiet=FALSE)
 RCPsamp_fin <- regimix(form.RCP=form, form.spp=~Season, 
-                     nRCP=nRCP_best, data=rcp_data, dist="NegBin", inits = unlist( nRCPs_samp[[nRCP_best]][[3]]$coef), control=control)
+                     nRCP=nRCP_best, data=rcp_data, dist="NegBin", inits = inits_best, control=control)
 
 # Clean-up workspace
-#rm(RCPsamp_BICs,RCPsamp_minPosteriorSites, RCPsamp_ObviouslyBad, RCPsamp_minBICs, RCPsamp_goodun, control)
+rm(RCPsamp_BICs,RCPsamp_minPosteriorSites, RCPsamp_ObviouslyBad, RCPsamp_minBICs, RCPsamp_goodun, control)
 
 # Plot model diagnostics
 # ======================
+# To do:  Add code to save figures to pdf files
 # Residual plots
-plot.regimix(RCPsamp_fin, type="RQR", fitted.scale="log") 
+plot.regimix(RCPsamp_fin, type="RQR", fitted.scale="log")
 
 # Cooks Distance Plots
-# Takes a while
-# tmp <- stability.regimix(RCPsamp_fin, oosSizeRange=c(1,2,3,4,5,6,7,8,9,10,20,30,40,50), mc.cores=1, times=RCPsamp_fin$n, doPlot=FALSE) # original
-tmp <- stability.regimix(RCPsamp_fin, oosSizeRange=c(1,2,5,10,20), mc.cores=1, times=RCPsamp_fin$n, doPlot=FALSE) # simplified
-plot( tmp, minWidth=2, ncuts=111 ) # I can't see the solid line across the plot that is in the examples!!!
+# Takes a while...
+tmp <- stability.regimix(RCPsamp_fin, oosSizeRange=c(1,2,3,4,5,6,7,8,9,10,20,30,40,50), mc.cores=1, times=RCPsamp_fin$n, doPlot=FALSE) # original
+#tmp <- stability.regimix(RCPsamp_fin, oosSizeRange=c(1,2,5,10,20), mc.cores=1, times=RCPsamp_fin$n, doPlot=FALSE) # simplified, but missing horizontal line
+plot( tmp, minWidth=2, ncuts=111 ) 
 
 saveRDS(tmp, file="Fish.Cooks.Dist.rds")
 tmp <- readRDS("Fish.Cooks.Dist.rds")
 
-# Examine dispersion parameter for negative Binomial  --- would this change with a Bernoulli distribution
+# Examine dispersion parameter for negative Binomial  --- would this change with a Bernoulli distribution?
+par(mfrow=c(1,1))
 hist(RCPsamp_fin$coefs$disp, xlab="Dispersion Parameter", 
      main="Negative Binomial Model", col="grey", cex.main=0.8, cex=0.8, cex.lab=0.8 )
 
-# Generate bootstrap estimates of parameters --- bootstrap is slooow
+# Generate bootstrap estimates of parameters
+# ==========================================
+# Warning --- bootstrap is slooow
 # rcpsamp_boots<-regiboot(RCPsamp_fin, type="BayesBoot", nboot=1000, mc.cores=1) # original code
 rcpsamp_boots<-regiboot(RCPsamp_fin, type="BayesBoot", nboot=100, mc.cores=1) # simplified
 
-#### Average, SD and CI of species abundances in each  RCP
-#for some reason will(?) produce warnings.
-# Function from RCP_Helper_Functions.R using tidyr()
+# Evaluate sampling factor effect
+# ===============================
+# ??Sp_abund_all()
+# Calculate average, SD and CI of species abundances in each  RCP
+# Sp_abund_all(bootstrap object) --- FUNCTION IS FOR FISH EXAMPLE --- NEED TO REWRITE!!!
 RCP_abund_samp<-Sp_abund_all(rcpsamp_boots)
 
-#Get autumn values and make pretty
-aut_samp<-as.data.frame(matrix(data=paste0(sprintf("%.2f", round(RCP_abund_samp$autumn$mean,2)), " (", sprintf("%.2f",round(RCP_abund_samp$autumn$sd,2)), ")"),
+# Get autumn values and make pretty
+aut_samp <- as.data.frame(matrix(data=paste0(sprintf("%.2f", round(RCP_abund_samp$autumn$mean,2)), " (", sprintf("%.2f",round(RCP_abund_samp$autumn$sd,2)), ")"),
                                ncol=3, nrow=15, byrow=TRUE))
-names(aut_samp)<-paste0("RCP", 1:3)
-rownames(aut_samp)<- gsub("."," ", species, fixed=TRUE)
+names(aut_samp) <- paste0("RCP", 1:3)
+rownames(aut_samp) <- gsub("."," ", species, fixed=TRUE)
 
+# ??sampling_dotplot2()
+# Plot of sampling factor effects
+sampling_dotplot2(RCPsamp_fin,rcpsamp_boots,legend_fact=c("Spring", "Summer"), col=c("black", "red"), lty=c(1,2))
+
+# Spatial Predictions
+# ===================
+RCPsamp_SpPreds<-predict.regimix(object=RCPsamp_fin, object2=rcpsamp_boots, newdata=rcp_poly_pred)
+# ??predict_maps2_SDF2()
+# Plot RCP predictions
+predict_maps2_SDF2(RCPsamp_SpPreds, pred_space=pred_space_rcp, pred_crop=pred_masked, nRCP=3)
+
+# To do:
+# Is there a way to export predictions to a shapefile?
 
